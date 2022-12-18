fe4d6e2b293a81a904c0ca17d6fb96b696740a7d
boywonder3 <opeyemiisaacboywonder@gmail.com>
2022-12-18 09:38:12 +0100

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const datePickerbutton = document.querySelector('.date-picker-button')
+import { format, getUnixTime, fromUnixTime } from 'date-fns'
+
+
+const datePickerButton = document.querySelector('.date-picker-button')
+const datePicker = document.querySelector('.date-picker')
+const datePickerHeaderText = document.querySelector('.current-month')
+
+
+datePickerButton.addEventListener('click', e => {
+    datePicker.classList.toggle('show')
+    const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
+    setupDatePicker(selectedDate)
+})
+
+function setDate(date) {
+     datePickerButton.innerText = format(date, 'MMMM do, yyyy')
+     datePickerButton.dataset.selectedDate = getUnixTime(date)
+}
+
+function setupDatePicker(selectedDate) {
+    datePickerHeaderText.innerText = format(selectedDate, 'MMMM - yyyy')
+    setupMonthButtons(selectedDate)
+}
+
+setDate(new Date())
