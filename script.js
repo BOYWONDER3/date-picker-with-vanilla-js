1f387799639395dca2b3cd776ea1bb084abc1ff8
boywonder3 <opeyemiisaacboywonder@gmail.com>
2022-12-19 11:23:34 +0100

    third commit

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-import { format, getUnixTime, fromUnixTime } from 'date-fns'
-
+import { format, getUnixTime, fromUnixTime, addMonths, subMonths } from 'date-fns'
+ 
 
 const datePickerButton = document.querySelector('.date-picker-button')
 const datePicker = document.querySelector('.date-picker')
 const datePickerHeaderText = document.querySelector('.current-month')
+const previousMonthButton = document.querySelector('.prev-month-button')
+const nextMonthButton = document.querySelector('.next-month-button')
+let currentDate = new Date()
 
 
 datePickerButton.addEventListener('click', e => {
     datePicker.classList.toggle('show')
     const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
+    currentDate = selectedDate
     setupDatePicker(selectedDate)
 })
 
@@ -17,9 +21,19 @@ function setDate(date) {
      datePickerButton.dataset.selectedDate = getUnixTime(date)
 }
 
-function setupDatePicker(selectedDate) {
-    datePickerHeaderText.innerText = format(selectedDate, 'MMMM - yyyy')
-    setupMonthButtons(selectedDate)
+function setupDatePicker() {
+    datePickerHeaderText.innerText = format(currentDate, 'MMMM - yyyy')
 }
+
+    nextMonthButton.addEventListener('click', e => {
+        currentDate = addMonths(currentDate, 1)
+        setupDatePicker()
+    })
+
+    previousMonthButton.addEventListener('click', e => {
+        currentDate = subMonths(currentDate, 1)
+        setupDatePicker()
+    })
+
 
 setDate(new Date())
